e71262118782caee2c744a52eca94f8aa390ebf1
luis henrique <luislopesteo@hotmail.com>
2023-05-09 09:40:02 -0300

    change order of low-cal and cal in protocol

--- Firmware/flash_memory_filling.c
+++ Firmware/flash_memory_filling.c
@@ -13,10 +13,10 @@ void memory_initialize(uint8_t TAG) {
     if (FLASH_ReadWord(PREAMBLE_ADDR) != PREAMBLE) {
         FLASH_WriteWord(PREAMBLE_ADDR, ramBuff, PREAMBLE);
         FLASH_WriteWord(STATUS_ADDR, ramBuff, STATUS_DEFAULT);
-        FLASH_WriteWord(CAL_MSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][0]);
-        FLASH_WriteWord(CAL_LSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][1]);
-        FLASH_WriteWord(LOW_CAL_MSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][2]);
-        FLASH_WriteWord(LOW_CAL_LSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][3]);
+        FLASH_WriteWord(LOW_CAL_MSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][0]);
+        FLASH_WriteWord(LOW_CAL_LSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][1]);
+        FLASH_WriteWord(CAL_MSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][2]);
+        FLASH_WriteWord(CAL_LSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][3]);       
         FLASH_WriteWord(HIGH_CAL_MSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][4]);
         FLASH_WriteWord(HIGH_CAL_LSB_ADDR, ramBuff, (uint16_t) default_in_flash[TAG - 1][5]);
         FLASH_WriteWord(LED_DATA_ADDR, ramBuff, 0x0000);
@@ -27,18 +27,18 @@ void memory_initialize(uint8_t TAG) {
 void data_recv_handler(void) {
 
 
+    if (CMD_LOW_CAL & i2cDataRead[CMD_I2C_POS]) {
+        FLASH_WriteWord(STATUS_ADDR, ramBuff, (uint16_t) CMD_LOW_CAL | (uint8_t) FLASH_ReadWord((uint16_t) STATUS_ADDR));
+
+        FLASH_WriteWord(LOW_CAL_MSB_ADDR, ramBuff, (uint16_t) i2cDataRead[LOW_CAL_MSB_I2C_POS]); // saves cal data
+        FLASH_WriteWord(LOW_CAL_LSB_ADDR, ramBuff, (uint16_t) i2cDataRead[LOW_CAL_LSB_I2C_POS]); //saves cal data
+    }
     if (CMD_CAL & i2cDataRead[CMD_I2C_POS]) {
 
         FLASH_WriteWord(STATUS_ADDR, ramBuff, (uint16_t) CMD_CAL | (uint8_t) FLASH_ReadWord((uint16_t) STATUS_ADDR)); // saves status
 
         FLASH_WriteWord(CAL_MSB_ADDR, ramBuff, (uint16_t) i2cDataRead[CAL_MSB_I2C_POS]); // saves cal data
         FLASH_WriteWord(CAL_LSB_ADDR, ramBuff, (uint16_t) i2cDataRead[CAL_LSB_I2C_POS]); //saves cal data
-    }
-    if (CMD_LOW_CAL & i2cDataRead[CMD_I2C_POS]) {
-        FLASH_WriteWord(STATUS_ADDR, ramBuff, (uint16_t) CMD_LOW_CAL | (uint8_t) FLASH_ReadWord((uint16_t) STATUS_ADDR));
-
-        FLASH_WriteWord(LOW_CAL_MSB_ADDR, ramBuff, (uint16_t) i2cDataRead[LOW_CAL_MSB_I2C_POS]); // saves cal data
-        FLASH_WriteWord(LOW_CAL_LSB_ADDR, ramBuff, (uint16_t) i2cDataRead[LOW_CAL_LSB_I2C_POS]); //saves cal data
     }
     if (CMD_HIGH_CAL & i2cDataRead[CMD_I2C_POS]) {
         FLASH_WriteWord(STATUS_ADDR, ramBuff, (uint16_t) CMD_HIGH_CAL | (uint8_t) FLASH_ReadWord((uint16_t) STATUS_ADDR));
@@ -58,10 +58,10 @@ void data_send_handler(void) {
     
     i2cDataWrite[STATUS_I2C_POS] = (uint8_t) FLASH_ReadWord(STATUS_ADDR);
     i2cDataWrite[BOARD_TYPE_I2C_POS] = (uint8_t) FLASH_ReadWord(BOARD_TYPE_ADDR);
-    i2cDataWrite[CAL_MSB_I2C_POS] = (uint8_t) FLASH_ReadWord(CAL_MSB_ADDR);
-    i2cDataWrite[CAL_LSB_I2C_POS] = (uint8_t) FLASH_ReadWord(CAL_LSB_ADDR);
     i2cDataWrite[LOW_CAL_MSB_I2C_POS] = (uint8_t) FLASH_ReadWord(LOW_CAL_MSB_ADDR);
     i2cDataWrite[LOW_CAL_LSB_I2C_POS] = (uint8_t) FLASH_ReadWord(LOW_CAL_LSB_ADDR);
+    i2cDataWrite[CAL_MSB_I2C_POS] = (uint8_t) FLASH_ReadWord(CAL_MSB_ADDR);
+    i2cDataWrite[CAL_LSB_I2C_POS] = (uint8_t) FLASH_ReadWord(CAL_LSB_ADDR);
     i2cDataWrite[HIGH_CAL_MSB_I2C_POS] = (uint8_t) FLASH_ReadWord(HIGH_CAL_MSB_ADDR);
     i2cDataWrite[HIGH_CAL_LSB_I2C_POS] = (uint8_t) FLASH_ReadWord(HIGH_CAL_LSB_ADDR);
     i2cDataWrite[LED_DATA_I2C_POS] = (uint8_t) FLASH_ReadWord(LED_DATA_ADDR);
--- Firmware/flash_memory_filling.h
+++ Firmware/flash_memory_filling.h
@@ -4,27 +4,27 @@
 #define STATUS_ARRAY_ADDR   0x780 
 #define PREAMBLE_ADDR       0x780
 #define STATUS_ADDR         0x781
-#define CAL_MSB_ADDR        0x782
-#define CAL_LSB_ADDR        0x783
-#define LOW_CAL_MSB_ADDR    0x784
-#define LOW_CAL_LSB_ADDR    0x785
+#define LOW_CAL_MSB_ADDR    0x782 
+#define LOW_CAL_LSB_ADDR    0x783 
+#define CAL_MSB_ADDR        0x784  
+#define CAL_LSB_ADDR        0x785 
 #define HIGH_CAL_MSB_ADDR   0x786
 #define HIGH_CAL_LSB_ADDR   0x787
 #define LED_DATA_ADDR       0x788
 #define BOARD_TYPE_ADDR     0x789
 
-#define CMD_CAL                 0x80
-#define CMD_LOW_CAL             0x40
+#define CMD_LOW_CAL             0x80
+#define CMD_CAL                 0x40
 #define CMD_HIGH_CAL            0x20
 #define CMD_LED                 0x10
 
 #define CMD_I2C_POS             0
 #define STATUS_I2C_POS          0
 #define BOARD_TYPE_I2C_POS      1
-#define CAL_MSB_I2C_POS         2
-#define CAL_LSB_I2C_POS         3
-#define LOW_CAL_MSB_I2C_POS     4
-#define LOW_CAL_LSB_I2C_POS     5
+#define LOW_CAL_MSB_I2C_POS     2
+#define LOW_CAL_LSB_I2C_POS     3
+#define CAL_MSB_I2C_POS         4
+#define CAL_LSB_I2C_POS         5
 #define HIGH_CAL_MSB_I2C_POS    6
 #define HIGH_CAL_LSB_I2C_POS    7
 #define LED_DATA_I2C_POS        8
